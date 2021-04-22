410e39fc250eda6d1269992f4862c281397bbbb0
CVBertrandCardon <bertrandcardondel@gmail.com>
2021-04-22 15:27:53 +0200

    refactoprofile

--- src/components/CvPage/index.js
+++ src/components/CvPage/index.js
@@ -3,8 +3,8 @@ import firebase from "../../utils/firebaseConfig";
 import UpdateDelete from "../UpdateDelete";
 import UpdateDeleteFormation from "../UpdateDeleteFormation/UpdateDeletFormation";
 import "./index.css";
-import UpdateDeleteProfile from '../Profile/UpdateDeleteProfile/index'
-import gifFinteen from '../../Assets/finteenfinalversion.gif'
+import UpdateDeleteProfile from "../Profile/UpdateDeleteProfile/index";
+import gifFinteen from "../../Assets/finteenfinalversion.gif";
 
 const CvPage = () => {
   const [quoteList, setQuoteList] = useState([]);
@@ -51,42 +51,52 @@ const CvPage = () => {
   }, []);
 
   return (
-    <div className="container-read">
-      <ul className="container-ul">
-        {profile &&
-          profile.map((item, index) =>
-            item.uid === "cLEV0torEifx0FsJ9W7hJ8MRrsO2" ? (
-              <UpdateDeleteProfile item={item} key={index} />
-            ) : (
-              ""
-            )
-          )}
-      </ul>
-      <div className="profile-container-skills">
-            <ul>
-              <h3>Hard skills</h3> 
-              <li>Front-end : React / Hooks / Redux / Javascript / Html / Css / Bootstrap</li>
-              <li>Back-end : Nodejs / Express / Postman</li>
-              <li>Database : SQL / Mysql / Sequelize</li>
-              <li>Developer :Github, GitLab / Visualstudio / Linux </li>
-            </ul>
-            <ul>
-              <h3>soft skills</h3> 
-              <li >Organization and project management (Scrum method)</li>
-              <li>Coaching/trainer</li>
-              <li>Commercial negotiation</li>
-            </ul>
-          </div>
-          <a href="https://portfolio-bertrand-cardon.webflow.io/" target="_blank" rel="noreferrer">
+    <div className="container-cvPage">
+      <div className="grid-profile">
+        <div className="container-user">
+          <ul className="container-ul">
+            {profile &&
+              profile.map((item, index) =>
+                item.uid === "cLEV0torEifx0FsJ9W7hJ8MRrsO2" ? (
+                  <UpdateDeleteProfile item={item} key={index} />
+                ) : (
+                  ""
+                )
+              )}
+          </ul>
+        </div>
+        <div className="profile-container-skills">
+          <ul>
+            <h3>Hard skills</h3>
+            <li>
+              Front-end : React / Hooks / Redux / Javascript / Html / Css /
+              Bootstrap
+            </li>
+            <li>Back-end : Nodejs / Express / Postman</li>
+            <li>Database : SQL / Mysql / Sequelize</li>
+            <li>Developer :Github, GitLab / Visualstudio / Linux </li>
+          </ul>
+          <ul>
+            <h3>soft skills</h3>
+            <li>Organization and project management (Scrum method)</li>
+            <li>Coaching/trainer</li>
+            <li>Commercial negotiation</li>
+          </ul>
+        </div>
+        <a
+          href="https://portfolio-bertrand-cardon.webflow.io/"
+          target="_blank"
+          rel="noreferrer"
+        >
           <div className="profile-container-portfolio">
-            <img src={gifFinteen} alt="gifFinteen" className="img-portfolio"/>
+            <img src={gifFinteen} alt="gifFinteen" className="img-portfolio" />
             <ul>
-              <h3>Portfolio</h3> 
-              <li >(Made in Weflow)</li>
+              <h3>Portfolio</h3>
+              <li>(Made in Weflow)</li>
             </ul>
           </div>
-          </a>
-
+        </a>
+      </div>
       <h3 className="title-experience">Professional experiences</h3>
       <ul className="container-ul">
         {quoteList &&
--- src/components/Profile/UpdateDeleteProfile/index.js
+++ src/components/Profile/UpdateDeleteProfile/index.js
@@ -3,7 +3,7 @@ import firebase from "../../../utils/firebaseConfig";
 import { UidContext } from "../../uidContext";
 import Button from "@material-ui/core/Button";
 import Textfield from "@material-ui/core/Textfield";
-import './index.css';
+import "./index.css";
 
 const UpdateDeleteProfile = ({ item }) => {
   const [update, setUpdate] = useState(false);
@@ -54,68 +54,63 @@ const UpdateDeleteProfile = ({ item }) => {
   };
 
   return (
-    <li className="li-list">
+    <div>
       {update === false && (
-        <div className="profile-information-container">
-          <div className="profile-container">
-            <img src={item.url} alt="profile" className="img-profile"/>
-            <ul>
-              <li className="name">{item.name}</li>
-              <li>{item.adress}</li>
-              <li>{item.mobile}</li>
-              <li>{item.email}</li>
-            </ul>
+        <div className="profile-container">
+          <img src={item.url} alt="profile" className="img-profile" />
+          <ul>
+            <li className="name">{item.name}</li>
+            <li>{item.adress}</li>
+            <li>{item.mobile}</li>
+            <li>{item.email}</li>
+          </ul>
+          {authorCheck() && (
+            <div className="button-container">
+              <Button
+                onClick={() => setUpdate(!update)}
+                variant="contained"
+                color="primary"
+              >
+                Update
+              </Button>
+              <Button onClick={deleteItem} variant="contained" color="primary">
+                Delete
+              </Button>
+            </div>
+          )}
+        </div>
+      )}
+      <li className="li-list">
+        {update && (
+          <div>
+            <Textfield
+              defaultValue={item.name}
+              onChange={(e) => setNameUpdate(e.target.value)}
+            />
+            <Textfield
+              type="text"
+              defaultValue={item.adress}
+              onChange={(e) => setAdressUpdate(e.target.value)}
+              id="standard-multiline-static"
+              label="Multiline"
+              multiline
+              rows={10}
+            />
+            <Textfield
+              defaultValue={item.mobile}
+              onChange={(e) => setMobileUpdate(e.target.value)}
+            />
+            <Textfield
+              defaultValue={item.email}
+              onChange={(e) => setEmailUpdate(e.target.value)}
+            />
+            <Button onClick={updateItem} variant="contained" color="primary">
+              valider
+            </Button>
           </div>
-            {authorCheck() && (
-              <div className="button-container">
-                <Button
-                  onClick={() => setUpdate(!update)}
-                  variant="contained"
-                  color="primary"
-                >
-                  Update
-                </Button>
-                <Button
-                  onClick={deleteItem}
-                  variant="contained"
-                  color="primary"
-                >
-                  Delete
-                </Button>
-              </div>
-            )}
-          
-        </div>
-      )}
-      {update && (
-        <div >
-          <Textfield
-            defaultValue={item.name}
-            onChange={(e) => setNameUpdate(e.target.value)}
-          />
-          <Textfield
-            type="text"
-            defaultValue={item.adress}
-            onChange={(e) => setAdressUpdate(e.target.value)}
-            id="standard-multiline-static"
-            label="Multiline"
-            multiline
-            rows={10}
-          />
-          <Textfield
-            defaultValue={item.mobile}
-            onChange={(e) => setMobileUpdate(e.target.value)}
-          />
-          <Textfield
-            defaultValue={item.email}
-            onChange={(e) => setEmailUpdate(e.target.value)}
-          />
-          <Button onClick={updateItem} variant="contained" color="primary">
-            valider
-          </Button>
-        </div>
-      )}
-    </li>
+        )}
+      </li>
+    </div>
   );
 };
 
